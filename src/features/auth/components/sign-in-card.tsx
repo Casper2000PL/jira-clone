bfe74b4e71e78b0b26f662b1e4e8a31ce36b7309
Casper2000PL <kacper.kozlicki2000@gmail.com>
2025-04-05 20:19:27 +0200

    6. Setting up Appwrite & database

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,34 +1,26 @@
-import { FcGoogle } from "react-icons/fc";
-import { FaGithub } from "react-icons/fa";
-
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-
+"use client";
 import { useForm } from "react-hook-form";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { useLogin } from "../api/use-login";
+import { loginSchema } from "../schemas";
 import { DottedSeparator } from "@/components/dotted-separator";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import {
   Form,
+  FormControl,
   FormField,
   FormItem,
-  FormControl,
   FormMessage,
 } from "@/components/ui/form";
+import { FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
 import Link from "next/link";
-import { loginSchema } from "../schemas";
-import { useLogin } from "../api/use-login";
 
 export const SignInCard = () => {
-  const { mutate } = useLogin();
-
+  const { mutate, isPending } = useLogin();
   const form = useForm<z.infer<typeof loginSchema>>({
     resolver: zodResolver(loginSchema),
     defaultValues: {
@@ -40,34 +32,30 @@ export const SignInCard = () => {
   const onSubmit = (values: z.infer<typeof loginSchema>) => {
     mutate({ json: values });
   };
-
   return (
     <Card className="w-full h-full md:w-[487px] border-none shadow-none">
-      <CardHeader className="flex items-center justify-center text-center p-7">
-        <CardTitle className="text-2xl">Welcome back</CardTitle>
-        <CardDescription className="text-sm text-muted-foreground">
-          Sign in to your account
-        </CardDescription>
+      <CardHeader className="flex items-center justify-center text-center ">
+        <CardTitle className="text-2xl">Welcome back!</CardTitle>
       </CardHeader>
-      <div className="px-7 mb-2">
+      <div className="px-7">
         <DottedSeparator />
       </div>
       <CardContent className="p-7">
         <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <form className="space-y-4" onSubmit={form.handleSubmit(onSubmit)}>
             <FormField
               name="email"
               control={form.control}
               render={({ field }) => (
-                <FormItem className="w-full">
+                <FormItem>
                   <FormControl>
                     <Input
+                      {...field}
                       type="email"
                       placeholder="Enter email address"
-                      {...field}
                     />
                   </FormControl>
-                  <FormMessage className="text-red-500" />
+                  <FormMessage />
                 </FormItem>
               )}
             />
@@ -75,19 +63,20 @@ export const SignInCard = () => {
               name="password"
               control={form.control}
               render={({ field }) => (
-                <FormItem className="w-full">
+                <FormItem>
                   <FormControl>
                     <Input
-                      type="password"
-                      placeholder="Enter password"
                       {...field}
+                      type="password"
+                      placeholder="Enter your password"
                     />
                   </FormControl>
-                  <FormMessage className="text-red-500" />
+                  <FormMessage />
                 </FormItem>
               )}
             />
-            <Button disabled={false} size="lg" className="w-full">
+
+            <Button disabled={isPending} size="lg" className="w-full">
               Login
             </Button>
           </form>
@@ -96,9 +85,9 @@ export const SignInCard = () => {
       <div className="px-7">
         <DottedSeparator />
       </div>
-      <CardContent className="flex p-7 flex-col gap-y-4">
+      <CardContent className="p-7 flex flex-col gap-y-4">
         <Button
-          disabled={false}
+          disabled={isPending}
           variant="secondary"
           size="lg"
           className="w-full"
@@ -107,7 +96,7 @@ export const SignInCard = () => {
           Login with Google
         </Button>
         <Button
-          disabled={false}
+          disabled={isPending}
           variant="secondary"
           size="lg"
           className="w-full"
@@ -116,14 +105,15 @@ export const SignInCard = () => {
           Login with Github
         </Button>
       </CardContent>
-      <div className="px-7 mb-2">
+      <div className="px-7">
         <DottedSeparator />
       </div>
-      <CardContent className="flex p-7 items-center justify-center">
-        Don&apos;t have an account?
-        <p className="ml-1">
+
+      <CardContent className="p-7 flex items-center justify-center">
+        <p>
+          Don&apos;t have an account?{" "}
           <Link href="/sign-up">
-            <span className="text-blue-700 hover:underline">Sign Up</span>
+            <span className="text-blue-700">&nbsp;Sign Up</span>
           </Link>
         </p>
       </CardContent>
