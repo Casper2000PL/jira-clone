d584d1d2967c3cf43bdd6c055a238022c5913254
Casper2000PL <kacper.kozlicki2000@gmail.com>
2025-07-20 22:10:18 +0200

    40. Responsive modal fix

--- src/components/responsive-modal.tsx
+++ src/components/responsive-modal.tsx
@@ -14,12 +14,11 @@ const ResponsiveModal = ({
   onOpenChange,
 }: ResponsiveModalProps) => {
   const isDesktop = useMedia("(min-width: 1024px)", true);
-  const isMobile = useMedia("(max-width: 1024px)", false);
 
   if (isDesktop) {
     return (
       <Dialog open={open} onOpenChange={onOpenChange}>
-        <DialogContent className="w-full sm:max-w-lg border-none overflow-y-auto hide-scrollbar max-h-[85vh]">
+        <DialogContent className="w-full sm:max-w-lg p-0 border-none overflow-y-auto hide-scrollbar max-h-[85vh]">
           {children}
         </DialogContent>
       </Dialog>
@@ -28,8 +27,10 @@ const ResponsiveModal = ({
 
   return (
     <Drawer open={open} onOpenChange={onOpenChange}>
-      <DrawerContent className="w-full sm:max-w-lg border-none overflow-y-auto hide-scrollbar max-h-[85vh]">
-        {children}
+      <DrawerContent>
+        <div className="overflow-y-auto hide-scrollbar max-h-[85vh]">
+          {children}
+        </div>
       </DrawerContent>
     </Drawer>
   );
